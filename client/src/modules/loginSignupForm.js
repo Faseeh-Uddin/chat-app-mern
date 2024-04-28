61a13b4b28815fafdb8f35593b93af7bee1c1929
Faseeh Uddin Shazil <shazilfaseehuddin@gmail.com>
2024-04-28 16:37:51 +0500

    Added Basic structure of Login Signup Form

---
+++ client/src/modules/loginSignupForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+const loginSignupForm = () => {
+  return <div className="bg-[#515253] w-[400px] h-[600px] shadow-lg rounded-lg flex flex-col justify-center items-center">
+    <div className="text-4xl font-extrabold">Welcome</div>
+    <div className="text font-light">Sign up now to get started</div>
+  </div>;
+};
+
+export default loginSignupForm;
