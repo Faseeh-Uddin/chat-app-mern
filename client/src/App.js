94bd6198e230af04a2f1c52f60bac7197e340db3
Faseeh Uddin Shazil <shazilfaseehuddin@gmail.com>
2024-05-02 18:01:26 +0500

    Added Routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,40 @@ import "./App.css";
 
 import LoginSignupForm from "./modules/loginSignupForm";
 import Dashboard from "./modules/dashboard";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+const ProtectedRoute =({children})=>{
+  const isLoggedIn = localStorage.getItem('user:token') !== null;
+  const location = useLocation();
+
+  // console.log("isLoggedIn: ", isLoggedIn);
+
+  if(!isLoggedIn && location.pathname === "/"){
+    return <Navigate to={'users/sign_in'} />
+  }
+  else if(isLoggedIn && ["/users/sign_in", "/users/sign_up"].includes(location.pathname)) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+}
 
 function App() {
   return (
     <div className="bg-dark-gray h-screen flex justify-center items-center">
-      {/* <LoginSignupForm /> */}
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<ProtectedRoute><Dashboard/></ProtectedRoute> } />
+        
+        <Route path="/users/sign_in" element={
+        <ProtectedRoute>
+          <LoginSignupForm isSignInPage={true} />
+        </ProtectedRoute>} />
+
+        <Route path="/users/sign_up" element={
+        <ProtectedRoute>
+          <LoginSignupForm isSignInPage={false}/>
+        </ProtectedRoute>} />
+      </Routes>
     </div>
   );
 }
--- client/src/modules/loginSignupForm.js
+++ client/src/modules/loginSignupForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Input from "../components/input";
 import Button from "../components/button";
+import { useNavigate } from "react-router-dom";
 const LoginSignupForm = ({isSignInPage=true}) => {
   const [data, setData] = useState({
     ...(!isSignInPage && {
@@ -10,19 +11,20 @@ const LoginSignupForm = ({isSignInPage=true}) => {
     password: ''
   })
 
+  const navigate = new useNavigate();
   // console.log(data)
   return <div className="bg-light-gray w-[60vh] h-[80vh] shadow-lg rounded-lg flex flex-col justify-center items-center">
     <div className="text-4xl font-extrabold text-[#e8eaed]">Welcome {isSignInPage && 'Back'}</div>
     <div className="text font-light text-light mb-8">{isSignInPage ? 'Sign In to explore' : 'Sign up now to get started'}</div>
     
     <form onSubmit={(e)=>{e.preventDefault(); console.log('Submitted')}} className="flex flex-col items-center w-full">
-      {!isSignInPage && <Input label="Full name" name="name" placeholder="Enter your full name" className="mb-2" value={data.fullName} onChange={(e) => setData({...data, fullName: e.target.value}) } />}
-      <Input label="Email" name="email" type="email" placeholder="Enter your email" className="mb-2" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
-      <Input label="Password" name="password" type="password" placeholder="Enter your password" className="mb-8" value={data.password} onChange={(e)=> setData({...data, password: e.target.value})} />
+      {!isSignInPage && <Input label="Full name" name="name" placeholder="Enter your full name" className="w-3/4" inputClassName="mb-2" value={data.fullName} onChange={(e) => setData({...data, fullName: e.target.value}) } />}
+      <Input label="Email" name="email" type="email" placeholder="Enter your email" className="w-3/4" inputClassName="mb-2" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+      <Input label="Password" name="password" type="password" placeholder="Enter your password" className="w-3/4" inputClassName="mb-8" value={data.password} onChange={(e)=> setData({...data, password: e.target.value})} />
       <Button label="Submit" type="submit" className="w-3/4 mb-2"/>
     </form>
     
-    <div className="text-sm font-light text-light">{isSignInPage ? "Don't have an account?" : "Already have an account?"}<span className="cursor-pointer underline px-1">{isSignInPage ? 'Sign up' : 'Sign in'}</span></div>
+    <div className="text-sm font-light text-light">{isSignInPage ? "Don't have an account?" : "Already have an account?"}<span className="cursor-pointer underline px-1" onClick={() => {navigate(`/users/${isSignInPage ? 'sign_up' : 'sign_in'}`)}}>{isSignInPage ? 'Sign up' : 'Sign in'}</span></div>
 
   </div>;
 };
